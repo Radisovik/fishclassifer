5aa9d6144f445ea3116d27bc1cf27fc5113a280a
ehubbard <eric.hubbard@gmail.com>
2022-11-22 22:44:37 -0800

    Shows the frames of a clip... left right arrows do stuff

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,69 +15,119 @@ import java.io.FileInputStream
 import java.io.IOException
 import java.io.InputStream
 import java.net.URL
+import java.util.*
 import javax.imageio.ImageIO
-import javax.swing.JButton
-import javax.swing.JFrame
-import javax.swing.JPanel
-import javax.swing.SwingUtilities
+import javax.swing.*
+import kotlin.collections.ArrayList
 
 
 object MainFrame : JFrame("Fish classifier") {
     val classes = ArrayList<Classified>()
-    val center =  ImagePanel()
+    val center =  ImagePanel(Dimension(320,180))
     val south = JPanel()
     init {
         contentPane.layout = BorderLayout()
         contentPane.add(center, BorderLayout.CENTER)
         contentPane.add(south, BorderLayout.SOUTH)
         south.layout = FlowLayout()
-        this.setSize(1280,720)
+        this.setSize(1500,900)
         this.setLocationRelativeTo(null)
         setFocusable(true)
-
-        classes.add(Classified("Salmon"))
+        classes.add(Classified("Nothing"))
         classes.add(Classified("Debris"))
+        classes.add(Classified("Shadows"))
         classes.add(Classified("Beaver"))
+        classes.add(Classified("Salmon"))
         classes.add(Classified("Otter"))
         classes.add(Classified("Other fish"))
-        classes.add(Classified("Shadows"))
-        classes.forEach { south.add(it.button) }
+        classes.forEach { south.add(it.panel) }
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         addKeyListener(object : KeyAdapter() {
             override fun keyPressed(e: KeyEvent) {
                 when (e.keyCode) {
-                    KeyEvent.VK_LEFT ->
+                    KeyEvent.VK_LEFT -> {
                         center.prev()
-
-                    KeyEvent.VK_RIGHT ->
+                        return
+                    }
+                    KeyEvent.VK_RIGHT -> {
                         center.next()
+                        return
+                    }
+                    in 49..57 -> {
+                        val idx = e.keyCode - 48
+                        classes.forEach{
+                            if (it.keyIdx == idx) {
+                                val removeCurrent = center.removeCurrent()
+                                it.imagePanel.add(removeCurrent)
+                                println("You classiefied as? ${it.button.text}")
+                                if (center.count()==0) {
+                                    println("done with clip.. onto the next one")
+                                }
+                            }
+                        }
+                        return
+                    }
                 }
-               // println("You pressed ${e.keyCode}")
+                println("You pressed ${e.keyCode}")
             }
         })
 
     }
 
 }
-
+var classifiedCount=1
 class Classified(name:String) : JPanel() {
     val button: JButton = JButton()
+    val imagePanel = ImagePanel(Dimension(1280, 720))
+    val panel = JPanel()
+    val keyIdx = classifiedCount++
     init {
-        button.text = name
+        panel.layout = BorderLayout()
+        panel.add(imagePanel, BorderLayout.CENTER)
+        panel.add(button, BorderLayout.SOUTH)
+        button.text = "$name ($keyIdx)"
     }
 }
 
-class ImagePanel : JPanel() {
+class ImagePanel(size:Dimension) : JPanel() {
     private var idx =0
     private val images = ArrayList<BufferedImage>()
     init {
+        setSize(size)
     }
+
+    fun count():Int {
+        return images.size
+    }
+
     override fun paint(g: Graphics) {
         super.paint(g)
         if(images.size ==0) {
             return
         }
         g.drawImage(images[idx], 0,0, size.width, size.height, null)
+    }
+
+    fun removeCurrent() :BufferedImage {
+        val removeAt = images.removeAt(idx)
+        clampIndex()
+        return removeAt
+    }
+
+    fun add(image : BufferedImage) {
+        images.add(image)
+        clampIndex()
+    }
+
+    fun clampIndex() {
+        if (idx <0 ) {
+            idx =0
+        }
+        if (idx >=images.size) {
+            idx = images.size-1
+        }
+        SwingUtilities.invokeLater { this.repaint() }
+
     }
 
     fun images(images:ArrayList<BufferedImage>) {
@@ -89,22 +139,31 @@ class ImagePanel : JPanel() {
 
     fun next() {
         idx++
-        if (idx >= images.size) {
-            idx--
-        }
-        SwingUtilities.invokeLater { this.repaint() }
+        clampIndex()
     }
 
     fun prev() {
         idx--
-        if (idx <0) {
-            idx=0
-        }
-        SwingUtilities.invokeLater { this.repaint() }
+        clampIndex()
     }
 
 }
 
+fun classes(): ArrayList<String> {
+    val strings = arrayOf(
+        "Beaver",
+        "Salmon",
+        "Rain",
+        "Insect",
+        "Human",
+        "Bird",
+        "Human shadow",
+        "Other",
+    )
+    val rtn = ArrayList<String>()
+    Arrays.stream(strings).forEach { rtn.add(it) }
+    return rtn
+}
 
 fun main(args: Array<String>) {
     val options: FirebaseOptions = FirebaseOptions.builder()
@@ -129,9 +188,7 @@ fun main(args: Array<String>) {
         .limit(1)
         .get().get()
 
-    val classes = ArrayList<String>()
-    classes.add("Beaver")
-    classes.add("Salmon")
+    val classes = classes()
 
     val converter = Java2DFrameConverter()
     videoFilesWithClips.documents.forEach{ it ->
